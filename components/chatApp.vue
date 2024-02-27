aaea05ccd7bc65aa71266f6c039aefa455356555
Tam <mthanimr24@gmail.com>
2024-02-27 11:43:42 +0000

    Chat app can check if user is signed in

---
+++ components/chatApp.vue
@@ -0,0 +1,20 @@
+<script setup>
+        
+    const user = useCurrentUser();
+
+</script>
+
+<template>
+    <div>
+        <template v-if="user">
+            <div>{{ user.displayName }}</div>
+        </template>
+        <template v-else>
+            <div>sign in</div>
+        </template>
+    </div>
+</template>
+
+<style scoped>
+ 
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 
 
 <template>
-
+  <chatApp />
 </template>
 
 <style>
